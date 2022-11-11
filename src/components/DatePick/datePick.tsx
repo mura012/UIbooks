f24621ebe5a199b13735cbde6a59973cee4176be
github user mura012 <murakoudai@gmail.com>
2022-11-11 21:40:12 +0900

    ✨ mantineを使ってデイトピッカーを作成

---
+++ src/components/DatePick/datePick.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import { Center } from "src/Layouts/Center/center";
+import { DatePicker } from "@mantine/dates";
+
+export const DatePick = () => {
+  const [value, onChange] = useState<any>(new Date());
+  const [date, setDate] = useState<boolean>(false);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+    if (!value) {
+      setDate(true);
+      return;
+    }
+    setDate(false);
+  };
+
+  return (
+    <Center>
+      <h1>実際にはログインできません</h1>
+      <form onSubmit={handleSubmit}>
+        <DatePicker
+          placeholder="日付を選択"
+          label="Event date"
+          error={date ? "※必須項目です" : ""}
+          withAsterisk
+          value={value}
+          onChange={onChange}
+        />
+        <input type="text" />
+        <button>送信</button>
+      </form>
+    </Center>
+  );
+};
