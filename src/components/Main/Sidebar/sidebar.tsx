0a1e16c4cbb541246b5644ab704061b247d65405
github user mura012 <murakoudai@gmail.com>
2022-11-11 11:16:37 +0900

    mantineを使うために色々設定

--- src/components/Main/Sidebar/sidebar.tsx
+++ src/components/Main/Sidebar/sidebar.tsx
@@ -1,5 +1,10 @@
 import React from "react";
+import { Button } from "@mantine/core";
 
 export const Sidebar = () => {
-  return <aside className="basis-1/5 bg-red-100">サイドバー</aside>;
+  return (
+    <aside className="basis-1/5 bg-red-100">
+      <Button color="cyan">ぼたん</Button>
+    </aside>
+  );
 };
