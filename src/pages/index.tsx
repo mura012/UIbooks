2918df2cf2cf2b264a8dc35a0adf4fc2005a5439
github user mura012 <murakoudai@gmail.com>
2022-11-12 15:14:42 +0900

    タイトルを設定

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,19 @@
+import Head from "next/head";
 import { Footer } from "src/components/Footer";
 import { Header } from "src/components/Header";
 import { Main } from "src/components/Main";
 
 export default function Home() {
   return (
-    <div>
-      <Header />
-      <Main />
-      <Footer />
-    </div>
+    <>
+      <Head>
+        <title>ホームページ</title>
+      </Head>
+      <div>
+        <Header />
+        <Main />
+        <Footer />
+      </div>
+    </>
   );
 }
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import { Center } from "src/Layouts/Center/center";
 import { IconEye, IconEyeOff } from "@tabler/icons";
 import { Button, Input } from "@mantine/core";
+import Head from "next/head";
 
 const Login = () => {
   const [password, setPassword] = useState<boolean>(true);
@@ -12,35 +13,40 @@ const Login = () => {
   };
 
   return (
-    <Center>
-      <h1>実際にはログインできません</h1>
-      <form>
-        <div className="w-80 bg-gray-50 p-5 shadow-md">
-          <p>メールアドレス</p>
-          <Input type="text" autoComplete="off" />
-          <p className="mt-4">パスワード</p>
-          <div className="flex justify-between">
-            <Input
-              type={password ? "password" : "text"}
-              autoComplete="off"
-              radius="xs"
-              className="grow"
-            />
-            <Button
-              onClick={handleClick}
-              className="flex items-center"
-              variant="outline"
-              color="dark"
-              radius="xs"
-            >
-              {password ? <IconEye /> : <IconEyeOff />}
-            </Button>
+    <>
+      <Head>
+        <title>ログインページ</title>
+      </Head>
+      <Center>
+        <h1>実際にはログインできません</h1>
+        <form>
+          <div className="w-80 bg-gray-50 p-5 shadow-md">
+            <p>メールアドレス</p>
+            <Input type="text" autoComplete="off" />
+            <p className="mt-4">パスワード</p>
+            <div className="flex justify-between">
+              <Input
+                type={password ? "password" : "text"}
+                autoComplete="off"
+                radius="xs"
+                className="grow"
+              />
+              <Button
+                onClick={handleClick}
+                className="flex items-center"
+                variant="outline"
+                color="dark"
+                radius="xs"
+              >
+                {password ? <IconEye /> : <IconEyeOff />}
+              </Button>
+            </div>
+            <Button style={{ marginTop: "20px" }}>ログイン</Button>
           </div>
-          <Button style={{ marginTop: "20px" }}>ログイン</Button>
-        </div>
-        <Link href="/">戻る</Link>
-      </form>
-    </Center>
+          <Link href="/">戻る</Link>
+        </form>
+      </Center>
+    </>
   );
 };
 
