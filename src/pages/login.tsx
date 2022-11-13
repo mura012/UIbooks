7b384a76e000274c90b2a1110aed869a56f70794
github user mura012 <murakoudai@gmail.com>
2022-11-13 13:22:41 +0900

    ラベルで囲んだ

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -39,32 +39,36 @@ const Login = () => {
       </Head>
       <Center>
         <form className="w-80 bg-gray-50 p-5 shadow-md">
-          <p>メールアドレス</p>
-          <Input
-            type="email"
-            autoComplete="off"
-            ref={ref}
-            placeholder="sample@gmail.com"
-          />
-          <p className="text-red-500">{errorText}</p>
-          <p className="mt-4">パスワード</p>
-          <div className="flex justify-between">
+          <label>
+            <p>メールアドレス</p>
             <Input
-              type={password ? "password" : "text"}
+              type="email"
               autoComplete="off"
-              radius="xs"
-              className="grow"
+              ref={ref}
+              placeholder="sample@gmail.com"
             />
-            <Button
-              onClick={handleClick}
-              className="flex items-center border-0"
-              variant="outline"
-              color="dark"
-              radius="xs"
-            >
-              {password ? <IconEye /> : <IconEyeOff />}
-            </Button>
-          </div>
+            <p className="text-red-500">{errorText}</p>
+          </label>
+          <label>
+            <p className="mt-4">パスワード</p>
+            <div className="flex justify-between">
+              <Input
+                type={password ? "password" : "text"}
+                autoComplete="off"
+                radius="xs"
+                className="grow"
+              />
+              <Button
+                onClick={handleClick}
+                className="flex items-center border-0"
+                variant="outline"
+                color="dark"
+                radius="xs"
+              >
+                {password ? <IconEye /> : <IconEyeOff />}
+              </Button>
+            </div>
+          </label>
           <Button style={{ marginTop: "20px" }} onClick={handleLogin}>
             ログイン
           </Button>
