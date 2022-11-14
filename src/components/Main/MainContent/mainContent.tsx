332c88fda1ac046a426acc0f971572939f011fb6
github user mura012 <murakoudai@gmail.com>
2022-11-14 19:21:58 +0900

    今は使わないので、消した

--- src/components/Main/MainContent/mainContent.tsx
+++ src/components/Main/MainContent/mainContent.tsx
@@ -1,10 +1,5 @@
 import React from "react";
-import { Rating } from "src/UI/Rating";
 
 export const MainContent = () => {
-  return (
-    <main className="grow bg-green-100">
-      <Rating />
-    </main>
-  );
+  return <main className="grow bg-green-100">{/* <Rating /> */}</main>;
 };
