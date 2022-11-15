4f24cd09e334c2d6773366d1306d5d372f3792cf
github user mura012 <murakoudai@gmail.com>
2022-11-15 17:07:44 +0900

    戻るボタンを設置

--- src/pages/[id].tsx
+++ src/pages/[id].tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { useRouter } from "next/router";
 import React from "react";
 import { Layout } from "src/Layouts/Layout";
@@ -12,7 +13,8 @@ const BlogPage = () => {
     <Layout>
       <div className="flex w-full flex-col items-center justify-center">
         <h1>{item.title}</h1>
-        <p>{item.text}</p>
+        <p className="max-w-xl">{item.text}</p>
+        <Link href="/blog">もどる</Link>
       </div>
     </Layout>
   );
