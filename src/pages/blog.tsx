02d8aa7f8970daf6c378ee8503b711eb12da1859
github user mura012 <murakoudai@gmail.com>
2022-11-16 21:04:26 +0900

    画像が設定されていない時ようの画像の設定法を変更

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -10,22 +10,17 @@ const Blog = () => {
       <div className="flex w-full justify-center">
         <ul>
           {blogData.map((item) => {
+            if (!item.img) {
+              item.img = "/noimage.jpeg";
+            }
             return (
               <li key={item.id}>
                 <Link href={`/${item.id}`}>
                   <div className="mb-4 max-w-xl border border-solid p-3 shadow-lg">
                     <p className="font-bold">{item.title}</p>
                     <p className="line-clamp-3">{item.text}</p>
-                    {item.img ? (
-                      <Image src={item.img} width={30} height={30} alt="画像" />
-                    ) : (
-                      <Image
-                        src="/noimage.jpeg"
-                        width={30}
-                        height={30}
-                        alt="画像"
-                      />
-                    )}
+
+                    <Image src={item.img} width={30} height={30} alt="画像" />
 
                     <div>著者：{item.author}</div>
                   </div>
