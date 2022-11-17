508e51a2455fc8dc079049d7434ff91404847593
github user mura012 <murakoudai@gmail.com>
2022-11-17 18:55:36 +0900

    h3タグをクリックしてもinputやtextareaにフォーカスが当たるように、labelタグを追加

--- src/components/Review/review.tsx
+++ src/components/Review/review.tsx
@@ -4,12 +4,18 @@ import { Rating } from "src/UI/Rating";
 export const Review = () => {
   return (
     <div className="m-auto">
-      <h3>総合評価</h3>
-      <Rating />
-      <h3>レビュータイトル</h3>
-      <input type="text" />
-      <h3>レビュー本文</h3>
-      <textarea cols={50} rows={8}></textarea>
+      <label>
+        <h3>総合評価</h3>
+        <Rating />
+      </label>
+      <label>
+        <h3>レビュータイトル</h3>
+        <input type="text" />
+      </label>
+      <label>
+        <h3>レビュー本文</h3>
+        <textarea cols={50} rows={8}></textarea>
+      </label>
       <button className="flex items-end">送信</button>
     </div>
   );
