baa7410c4c98dd44e6489a1d007577f447b8064b
github user mura012 <murakoudai@gmail.com>
2022-11-21 18:59:27 +0900

    マウスをのせた時とはなした時のイベントを変更

--- src/Layouts/SpeechBubble/speechBubble.tsx
+++ src/Layouts/SpeechBubble/speechBubble.tsx
@@ -8,28 +8,28 @@ export const SpeechBubble = ({ children, title, text, position }: Props) => {
   const [showModal, setShowModal] = useState<boolean>(false);
   const mouseIn = () => {
     setTime(true);
-    console.log("マウスインした");
   };
   const mouseOut = () => {
-    setTime(false);
     setShowModal(false);
-    console.log("マウスアウトした");
   };
 
   useEffect(() => {
     if (time === true) {
       var timer = setTimeout(() => {
         setShowModal(true);
-        console.log("１びょうたった");
       }, 1000);
     }
-
     return () => {
       clearTimeout(timer);
     };
   }, [time]);
+
   return (
-    <div className={classes.group} onMouseOver={mouseIn} onMouseOut={mouseOut}>
+    <div
+      className={classes.group}
+      onMouseEnter={mouseIn}
+      onMouseLeave={mouseOut}
+    >
       {position === "top" && (
         <div
           className={classes.top}
