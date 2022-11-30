edbe6a2836f3be109adefa263d88c940c51bc073
mura012 <murakoudai@gmail.com>
2022-11-30 14:52:52 +0900

    バッジを作成

--- src/components/MainContent/mainContent.tsx
+++ src/components/MainContent/mainContent.tsx
@@ -1,17 +1,14 @@
 import React from "react";
-import { useToggle } from "src/Fooks/useToggle";
+import { Badge } from "src/UI/Badge";
 import { Modal, ModalChildren } from "src/UI/Modal";
-import { ToggleButton } from "src/UI/ToggleButton";
 
 export const MainContent = () => {
-  const { toggle, handleToggle } = useToggle();
   return (
     <main className="grow bg-green-100">
       <Modal>
         <ModalChildren title="children" body="ほんぶんあああああ" />
       </Modal>
-      <ToggleButton handleToggle={handleToggle} toggle={toggle} />
-      {toggle ? "on" : "off"}
+      <Badge bw="white">バッチ</Badge>
     </main>
   );
 };
